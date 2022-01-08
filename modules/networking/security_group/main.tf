262ffe36f427dee79e621b96c97179b3e7214ac3
suminhong <sci970180@gmail.com>
2022-01-08 21:32:21 +0900

    sg module2

--- modules/networking/security_group/main.tf
+++ modules/networking/security_group/main.tf
@@ -14,10 +14,10 @@ resource "aws_security_group" "sg" {
   vpc_id      = var.vpc_id
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(
